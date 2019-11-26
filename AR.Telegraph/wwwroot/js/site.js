d8a25a4c6c0f84eb1a67a427779f50a239e6c759
jazza-code <58073232+jazza-code@users.noreply.github.com>
2019-11-26 18:48:06 +0200

    update some change

--- AR.Telegraph/wwwroot/js/site.js
+++ AR.Telegraph/wwwroot/js/site.js
@@ -1,11 +1,23 @@
 ﻿$(function () {
     $('[data-toggle="tooltip"]').tooltip();
+    SetFooter();
+});
+$(document).on('blur keyup change click', function (e) {
+    SetFooter();
+});
+$(window).on('resize', function () {
+    SetFooter();
+});
+function SetFooter() {
     var docHeight = $(document).height();
     var windowHeight = $(window).height();
     if (docHeight <= windowHeight) {
-        $('.footer').css({'position': 'fixed', 'bottom': '0', 'left': '0','right': '0'});
+        $('.footer').css({ 'position': 'fixed', 'bottom': '0', 'left': '0', 'right': '0' });
     }
-});
+    else {
+        $('.footer').removeAttr('style');
+    }
+}
 function readImage(input, viewPictureId) {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
